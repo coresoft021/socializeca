1d844050262200a523f5d705a7b9f32a627c9e45
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-03-02 11:35:34 +0530

    Update public.js

--- server/routes/public.js
+++ server/routes/public.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-
+const greeting_1 = require("../models/admin_table");
 const dbcon_1 = require("./dbcon");
 const publicRouter = express_1.Router();
 exports.publicRouter = publicRouter;
@@ -10,7 +10,18 @@ exports.publicRouter = publicRouter;
 
 
 
-
+publicRouter.get('/list_greet', (request, response) => {
+    greeting_1.Tas_greeting.findAll({
+       
+    }).then(res => {
+        if (res) {
+            response.send(res);
+        }
+        else {
+            return response.json({ success: false, msg: 'Nothing found' });
+        }
+    });
+});
 publicRouter.get("/about", (request, response) => {
     response.json({
         Devoloper: "Premji Thankaji",
