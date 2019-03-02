4917d0e20e6ad9c706241d0e2001d61fc155622c
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-03-02 11:12:56 +0530

    Create public.js

---
+++ server/routes/public.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+
+const dbcon_1 = require("./dbcon");
+const publicRouter = express_1.Router();
+exports.publicRouter = publicRouter;
+const Op = dbcon_1.Sequelize.Op;
+
+
+
+
+
+publicRouter.get("/about", (request, response) => {
+    response.json({
+        Devoloper: "Premji Thankaji",
+        Framework: "A6.0 + sequlize + nodejs",
+    });
+});
+publicRouter.get("/server", (request, response) => {
+    response.json({ Status: "Running"
+    });
+});
+//# sourceMappingURL=public.js.map
