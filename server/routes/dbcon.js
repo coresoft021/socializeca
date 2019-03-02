2a015320c017afb25e204023dafe64bcdb740e4b
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-03-02 11:41:00 +0530

    Update dbcon.js

--- server/routes/dbcon.js
+++ server/routes/dbcon.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 exports.Sequelize = sequelize_1.Sequelize;
-var sequelize = new sequelize_1.Sequelize('postgres://eabenglqnnlxii:3895dc322b94f951b9e3eddc203744d5fcfa9c228c504b489cd1a96f875c74cd@ec2-54-235-193-34.compute-1.amazonaws.com:5432/d50eu6rmqqbp5r');
+var sequelize = new sequelize_1.Sequelize('postgres://zsepyqdiizsggz:7ce949b91d72a8efbe4a12bf22b4f642318a56fb0434e38fb5dfe75043cd8703@ec2-54-83-17-151.compute-1.amazonaws.com:5432/deq4aoucdv22oo');
 exports.sequelize = sequelize;
