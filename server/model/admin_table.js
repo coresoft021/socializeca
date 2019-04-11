40ef6d8ebcf2d8cc15850ac62f0ac98d65829ce1
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 17:38:57 +0530

    Update admin_table.js

--- server/model/admin_table.js
+++ server/model/admin_table.js
@@ -1,28 +1,28 @@
+
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dbcon_1 = require("../routes/dbcon");
-const Tas_admin = dbcon_1.sequelize.define('tas_admin', {
-       ID: {
-      type: dbcon_1.Sequelize.INTEGER(6),
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
+const Ad_min_ = dbcon_1.sequelize.define('admin', {
+    ID: {
+        type: dbcon_1.Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
     },
     USER_NAME: {
-      type: dbcon_1.Sequelize.STRING(25),
-      allowNull: true
+        type: dbcon_1.Sequelize.STRING,
+        allowNull: true
     },
-      PASSWORD: {
-      type: dbcon_1.Sequelize.STRING(25),
-      allowNull: true
+    PASSWORD: {
+        type: dbcon_1.Sequelize.STRING,
+        allowNull: true
     },
     IS_ADMIN: {
-      type: dbcon_1.Sequelize.Sequelize.BOOLEAN,
-      allowNull: true
+        type: dbcon_1.Sequelize.BOOLEAN,
+        allowNull: true
     },
-  
-  }, {
-    tableName: 'tas_admin',
-     timestamps: false
+}, {
+    tableName: 'so_admin',
+    timestamps: false
 });
-exports.Tas_admin = Tas_admin;
+exports.Ad_min = Ad_min_;
