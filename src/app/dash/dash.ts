9cf603eaaea1d78588c7ddc6862b24a838651c52
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 19:19:54 +0530

    Update dash.ts

--- src/app/dash/dash.ts
+++ src/app/dash/dash.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthGuard } from '../services/auth.guard';
+import  { main_model } from './model';
+import { DataService } from '../../services/data.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
@@ -10,17 +12,53 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class DashComponent  {
   
- view : boolean = false;
+arrayOfValues : any [] = [];
+view_model = new main_model([]);
+loop : number = 0;
+view : boolean = false;
+len : number  = 0;
+
+
  ngOnInit() {
-    this.get_subs()
-  }
+              this.get_customers_subs_list()
+             }
   constructor(private ag : AuthGuard,private _formBuilder: FormBuilder,private router: Router ) { }
   
  
-get_subs()
+get_customers_subs_list()
   {
-    
+     this.ds.get_customers_subs_list().subscribe((jsonData) => { this.getval2(jsonData)  
+                      },(err) => console.error(err),
+                       
+                      );
   } 
+  
+  getval2(s:any)
+{
+
+ 
+this.arrayOfValues = s
+this.len = this.arrayOfValues.length
+for (this.loop = 0; this.loop < this.len; this.loop++)
+ {
+  this.push();
+  this.view_model.item[this.loop].EMAIL = this.arrayOfValues[this.loop].EMAIL
+  this.view_model.item[this.loop].FIRST_NAME = this.arrayOfValues[this.loop].FIRST_NAME
+  this.view_model.item[this.loop].LAST_NAME = this.arrayOfValues[this.loop].LAST_NAME
+  this.view_model.item[this.loop].RATING_STAR = this.arrayOfValues[this.loop].RATING_STAR
+  this.view_model.item[this.loop].SI = this.loop + 1
+ }
+ 
+ this.view = true
+}
+push()
+{ 
+  this.view_model.item.push({"SI":0,"ID":0,"FIRST_NAME":'',"LAST_NAME":'',"EMAIL":'',"RATING_STAR":0})
+}
+
+
+
+  
    logout()
   {
     this.ag.token=false;
