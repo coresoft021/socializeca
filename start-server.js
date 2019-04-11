8920de20ad97d4fef0353cbc755bdbbaeca20312
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 18:02:18 +0530

    Update start-server.js

--- start-server.js
+++ start-server.js
@@ -1,29 +1,31 @@
 
 
- "use strict";
- Object.defineProperty(exports, "__esModule", { value: true });
- const express = require('express');
+const express = require('express');
+const path = require('path');
 const body_parser_1 = require("body-parser");
+const public_1 = require("./routes/public");
+var cors = require('cors');
+const app = express();
 
- const public_1 = require("./server/routes/public");
-
- var cors = require('cors');
- const app = express();
- const path = require('path');
-
- app.use(cors());
- app.use(body_parser_1.json());
+app.use(cors());
+app.use(body_parser_1.json());
 app.use(body_parser_1.urlencoded({ extended: true }));
 
- app.use("/api/public", public_1.publicRouter);
+app.use("/api/public", public_1.publicRouter);
 
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname , './dist/heroku-app-angular'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname , './dist/heroku-app-angular/index.html'));
- });
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,'./dist/heroku-app-angular/index.html'));
+});
+
  app.get('/api', function (req, res) {
     res.send('Hello World, How are you!');
  });
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
- app.listen(process.env.PORT || 8081);
+
