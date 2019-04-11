76a78e825489e2011ae4a4abfc21231add6010ee
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 16:30:12 +0530

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,14 +7,14 @@ import { AuthGuard } from './services/auth.guard';
 import { DashComponent } from './dash/dash';
 import { HomeComponents } from './home/home';
 import { LoginComponent } from './login/login';
-import { Bill_b2b } from './bill/b2b/main';
+
 
 
 const routes: Routes = [
     { path: '', component: HomeComponents },
     { path: 'login',component: LoginComponent },
     { path: 'dash', component: DashComponent,canActivate:[AuthGuard] },
-    { path: 'b2b', component: Bill_b2b,canActivate:[AuthGuard] },
+ 
     
     
 
