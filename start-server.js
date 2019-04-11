581845cb809ff1ed479b7773b764e9083f6c1b82
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 17:50:50 +0530

    Update start-server.js

--- start-server.js
+++ start-server.js
@@ -1,62 +1,62 @@
 
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const body_parser_1 = require("body-parser");
-const public_1 = require("./server/routes/public");
-var cors = require('cors');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 8080;
-app.use(cors());
-app.use(body_parser_1.json());
-app.use(body_parser_1.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
-app.use("/api/public", public_1.publicRouter);
-
-
-app.get('/api', function (req, res) {
-    res.send('Hello World, How are you!');
-});
-
-app.use(express.static(__dirname + '/dist/heroku-app-angular'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/heroku-app-angular/index.html'));
-});
-});
-app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
-});
-
-
-
-
-// //Install express server
-
 // "use strict";
 // Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express');
 // const body_parser_1 = require("body-parser");
-
-// const cdb_1 = require("./server/routes/cdb");
-
+// const public_1 = require("./server/routes/public");
 // var cors = require('cors');
 // const app = express();
 // const path = require('path');
-
+// const port = process.env.PORT || 8080;
 // app.use(cors());
 // app.use(body_parser_1.json());
 // app.use(body_parser_1.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + '/public'));
+// app.use("/api/public", public_1.publicRouter);
 
-// app.use("/api/cdb", public_1.cdbRouter);
+
+// app.get('/api', function (req, res) {
+//     res.send('Hello World, How are you!');
+// });
 
 // app.use(express.static(__dirname + '/dist/heroku-app-angular'));
 // app.get('/*', function (req, res) {
 //     res.sendFile(path.join(__dirname + '/dist/heroku-app-angular/index.html'));
 // });
-// app.get('/api', function (req, res) {
-//     res.send('Hello World, How are you!');
+// });
+// app.listen(port, function () {
+//     console.log(`Example app listening on port ${port}!`);
 // });
 
 
-// app.listen(process.env.PORT || 8081);
+
+
+// Install express server
+
+ "use strict";
+ Object.defineProperty(exports, "__esModule", { value: true });
+ const express = require('express');
+const body_parser_1 = require("body-parser");
+
+ const public_1 = require("./server/routes/public");
+
+ var cors = require('cors');
+ const app = express();
+ const path = require('path');
+
+ app.use(cors());
+ app.use(body_parser_1.json());
+app.use(body_parser_1.urlencoded({ extended: true }));
+
+ app.use("/api/public", public_1.publicRouter);
+
+app.use(express.static(__dirname , './dist/heroku-app-angular'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname , './dist/heroku-app-angular/index.html'));
+ });
+ app.get('/api', function (req, res) {
+    res.send('Hello World, How are you!');
+ });
+
+
+ app.listen(process.env.PORT || 8081);
