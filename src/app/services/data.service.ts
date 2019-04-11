928aa3eb6082325bdd13162dc190ebc4b238ad17
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2019-04-11 19:22:42 +0530

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -16,49 +16,19 @@ export class DataService {
 
     
     constructor(private http: Http) { }
-
-       post_invoice_b2b(model:any) 
-       {
-        let body= JSON.stringify(model); 
-                 console.log('body',body);
-      return this.http.post('https://coresoft.herokuapp.com/api/post/post_invoice_b2b', body, { headers: contentHeaders})
-            .map((res: Response) => res.json())
-            
-        } 
-
-   get_figure(model:any)
-  {
-    let body = JSON.stringify(model); 
-       console.log('body',body);
-      return this.http.post('https://coresoft.herokuapp.com/api/public/number_to_word',body,{headers : contentHeaders})  
-      .map((res: Response) => res.json())
-  }
-
-   get_invo_number_b2b()
-    {
-         return this.http.get('https://coresoft.herokuapp.com/api/public/get_invoice_number_b2b',{headers : contentHeaders})  
-         .map((res: Response) => res.json())
-    }
-
     
-    get_product_list()
+    
+        get_customers_subs_list()
     {
-       return this.http.get('https://coresoft.herokuapp.com/api/public/get_product_list',{headers : contentHeaders})  
-      .map((res: Response) => res.json())
-    }
-
-    get_reciept_number_b2b()
-    {
-      return this.http.get('http://localhost:8081/api/public/get_reciept_number_b2b',{headers : contentHeaders})  
+      return this.http.get('https://coresoft.herokuapp.com/api/public/get_subscription_list',{headers : contentHeaders})  
       .map((res: Response) => res.json())
     }
 
 
-   get_customer_list()
-   {
-     return this.http.get('https://coresoft.herokuapp.com/api/public/get_customer_list',{headers : contentHeaders})  
-      .map((res: Response) => res.json())
-   }
+
+
+
+
 
     adduser(model:any) {
 
